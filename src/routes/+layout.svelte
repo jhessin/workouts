33e19ddc92b1dddf0a4075fb18696cf01e7c414e
Jim Hessin <jhessin@gmail.com>
2023-02-16 16:36:39 -0700

    Revert "reactivity"
    This reverts commit 1996f48d6f68942d6984d2d4074c33cb086ec3ea.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import {init, user} from '$lib/firebase';
 	import {GoogleAuthProvider, signInWithPopup} from 'firebase/auth';
-	import {onMount} from 'svelte';
 	import {quintOut} from 'svelte/easing';
 	import {slide} from 'svelte/transition';
 	import 'w3-css/w3.css';
@@ -13,57 +12,49 @@
 
 	let sidebar: HTMLDivElement;
 	let showSidebar = false;
-	let alwaysShowSidebar: boolean = false;
-	onMount(() => {
-		alwaysShowSidebar = window.innerWidth >= 640;
-	});
 </script>
 
-<main>
-	{#if showSidebar || alwaysShowSidebar}
-		<div
-			class="w3-sidebar w3-bar-block w3-collapse w3-card w3-mobile"
-			transition:slide={{delay: 250, duration: 1000, easing: quintOut}}
-			style="width: 250px;"
-			bind:this={sidebar}
-		>
-			{#if !alwaysShowSidebar}
-				<button
-					class="w3-button w3-display-topright w3-xlarge"
-					on:click={() => (showSidebar = false)}
-				>
-					&times
-				</button>
-			{/if}
-			{#if !$user}
-				<button
-					class="w3-button w3-green w3-bar-item"
-					on:click={() => {
-						signInWithPopup(auth, new GoogleAuthProvider());
-					}}
-				>
-					Sign in with Google
-				</button>
-			{:else}
-				<button
-					class="w3-button w3-red w3-bar-item"
-					on:click={() => {
-						auth.signOut();
-					}}
-				>
-					Sign Out
-				</button>
-			{/if}
-		</div>
-	{/if}
-	<button
-		class="w3-button w3-xlarge"
-		on:click={() => (showSidebar = true)}
+{#if showSidebar}
+	<div
+		class="w3-sidebar w3-bar-block w3-collapse w3-card"
+		transition:slide={{delay: 250, duration: 1000, easing: quintOut}}
+		style="width: 250px;"
+		bind:this={sidebar}
 	>
-		&#9776;
-	</button>
-
-	<div class="w3-main w3-content">
-		<slot><!-- optional fallback --></slot>
+		<button
+			class="w3-button w3-display-topright w3-xlarge"
+			on:click={() => (showSidebar = false)}
+		>
+			&times
+		</button>
+		{#if !$user}
+			<button
+				class="w3-button w3-green w3-bar-item"
+				on:click={() => {
+					signInWithPopup(auth, new GoogleAuthProvider());
+				}}
+			>
+				Sign in with Google
+			</button>
+		{:else}
+			<button
+				class="w3-button w3-red w3-bar-item"
+				on:click={() => {
+					auth.signOut();
+				}}
+			>
+				Sign Out
+			</button>
+		{/if}
 	</div>
-</main>
+{/if}
+<button
+	class="w3-button w3-xlarge"
+	on:click={() => (showSidebar = true)}
+>
+	&#9776;
+</button>
+
+<div class="w3-main w3-content">
+	<slot><!-- optional fallback --></slot>
+</div>
